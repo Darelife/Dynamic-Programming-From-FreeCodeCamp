cc34fdce58ac53088a6ce5b765b488256f7876b0
itzp <67017000+Darelife@users.noreply.github.com>
2022-10-18 18:06:01 +0530

    finished the brute function for bestSum

--- Best Sum Algorithm Memoization/brute.py
+++ Best Sum Algorithm Memoization/brute.py
@@ -8,3 +8,79 @@
 #
 # example : bestSum(7, [5,3,4,7]) -> [7]
 #           7 = 7
+#
+#           bestSum(8, [2,3,5]) -> [3,5]
+#           3+5 = 8
+#
+#           bestSum(8, [1,4,5]) -> [4,4]
+#           4+4 = 8
+#
+#           bestSum(100, [1,2,5,25]) -> [25,25,25,25]
+#           25+25+25+25 = 100
+#
+#           bestSum(8, [1,4,5]) -> None
+#           no combination of 1,4,5 can add up to 8
+#
+
+#? MY SOLUTION
+
+#* Attempt 1
+
+# def bestSum(targetSum, numbers):
+#     if targetSum == 0: return []
+#     if targetSum < 0: return None
+#     answers = []
+#     ans = []
+#     for num in numbers:
+#         remainder = targetSum - num
+#         if bestSum(remainder, numbers) is not None:
+#             answers.append(num)
+#             ans.append(answers)
+#             answers = []
+#     if ans != [] or ans != None:
+#         ans.sort(key=len)
+#         print(ans)
+#         return ans[0]
+#     return None
+
+# print(bestSum(7, [5,3,4,7]))
+# print(bestSum(8, [2,3,5]))
+
+#* After watching the video
+
+def bestSum(targetSum, numbers):
+    if targetSum == 0: return []
+    if targetSum < 0: return None
+    shortestCombination = None
+    
+    for num in numbers:
+        remainder = targetSum - num
+        remainderResult = bestSum(remainder, numbers)
+        if remainderResult is not None:
+            remainderResult.append(num)
+            if shortestCombination is None or len(shortestCombination)>len(remainderResult):
+                shortestCombination = remainderResult
+
+    return shortestCombination #return is outside loop cuz we need to make sure that it's the shortest 
+    #combination that we return and not just the first combination that we find
+
+print(bestSum(7, [5,3,4,9]))
+
+
+
+#? FREECODECAMP SOLUTION FOR BRUTE FORCE
+
+def bestSum2(targetSum, numbers):
+    if targetSum == 0: return []
+    if targetSum < 0: return None
+    shortestCombination = None
+    for num in numbers:
+        remainder = targetSum - num
+        remainderResult = bestSum2(remainder, numbers)
+        if remainderResult is not None:
+            combination = remainderResult + [num]
+            if shortestCombination is None or len(combination) < len(shortestCombination):
+                shortestCombination = combination
+    return shortestCombination
+
+print(bestSum2(7, [5,3,4,7]))
